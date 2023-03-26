526e9ffffd4123ddf8deb9f815bf01ea79b33e1d
saumyatalwani <saumyaofficial007@gmail.com>
2023-03-26 09:29:42 +0530

    graphql config

--- config/env/plugins.js
+++ config/env/plugins.js
@@ -22,7 +22,7 @@ module.exports = ({ env }) => ({
       config: {
         endpoint: '/graphql',
         shadowCRUD: true,
-        playgroundAlways: false,
+        playgroundAlways: true,
         depthLimit: 7,
         amountLimit: 100,
         apolloServer: {
